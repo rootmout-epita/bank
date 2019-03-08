0fcfffb6ad0d40a7a486178eb55d91eb6c179ba9
Pierre KELBERT <pierre.kelbert@kleeft.fr>
2019-03-08 11:57:38 -0500

    Modification of AccountController...

--- src/Controller/AccountsController.php
+++ src/Controller/AccountsController.php
@@ -3,21 +3,40 @@
 namespace App\Controller;
 
 use App\Entity\Accounts;
+use App\Entity\User;
+use App\Repository\AccountsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccountsController extends AbstractController
 {
     /**
-     * @Route("/accounts", name="accounts")
+     * @var AccountsRepository
      */
-    public function index()
+    private $repository;
+
+    public function __construct(AccountsRepository $repository)
     {
-        $repository = $this->getDoctrine()->getRepository(Accounts::class);
-        dump($repository);
+        $this->repository = $repository;
+    }
+
+
+    /**
+     * @Route("/accounts", name="accounts")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        $user = $this->getUser();
+        $account = $this->repository->findBy(['Client' => $user->getId()]);
+        dump($account);
 
         return $this->render('accounts/index.html.twig', [
-            'controller_name' => 'AccountsController',
+            'accounts' => $account,
+            'user' => $user,
         ]);
     }
 }
